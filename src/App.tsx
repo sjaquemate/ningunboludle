cb48b5acd471f1141f009ec87311e13892e81ff9
me <me@gmail.com>
2025-10-04 22:04:21 +0200

    conexiones for later

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Boludle from './Boludle'
 import BuildInfo from './components/dev/BuildInfo'
-import Conexiones from './components/conexiones/Conexiones'
+// import Conexiones from './components/conexiones/Conexiones'
 
 function App() {
 
@@ -9,7 +9,7 @@ function App() {
     <BrowserRouter basename="/">
       <Routes>
         <Route path="/build/info" element={<BuildInfo />} />
-        <Route path="/conexiones" element={<Conexiones />} />
+        {/* <Route path="/conexiones" element={<Conexiones />} /> */}
 
         <Route path="/" element={<Boludle />} />
         <Route path="*" element={<Boludle />} />
